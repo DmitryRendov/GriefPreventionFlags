97392edd24a65d53dc1a2c9965a21306eaf1ff4c
iNko97 <26715474+iNko97@users.noreply.github.com>
2018-06-09 16:04:30 +0200

    Fix pom to not need local repo
    Merge other repositories for unified repo

--- src/me/ryanhamshire/GPFlags/FlagDef_NoItemPickup.java
+++ src/me/ryanhamshire/GPFlags/FlagDef_NoItemPickup.java
@@ -1,21 +1,29 @@
 package me.ryanhamshire.GPFlags;
 
+//import org.bukkit.entity.Entity;
+//import org.bukkit.entity.EntityType;
+//import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+//import org.bukkit.event.player.PlayerPickupItemEvent;
 
 public class FlagDef_NoItemPickup extends FlagDefinition
 {
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerPickupItem(PlayerPickupItemEvent event)
+    //где-то тут я лазил. PlayerPickupItemEvent -> EntityPickupItemEvent
+    public void onPlayerPickupItem(EntityPickupItemEvent event)
     {
-        Player player = event.getPlayer();
+    	if (event.getEntity() instanceof Player) 
+    	{
+    	 Player player = (Player)event.getEntity();	 
+         Flag flag = this.GetFlagInstanceAtLocation(player.getLocation(), player);
+         if(flag == null) return;
+         ((Cancellable) event).setCancelled(true);
+        }
         
-        Flag flag = this.GetFlagInstanceAtLocation(player.getLocation(), player);
-        if(flag == null) return;
-        
-        event.setCancelled(true);
     }
     
     public FlagDef_NoItemPickup(FlagManager manager, GPFlags plugin)
